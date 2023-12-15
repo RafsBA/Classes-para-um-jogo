71bc2c1835e95ce91c8678e67065969326429313
RafsBA <rafaelsant.oliv@gmail.com>
2023-12-15 11:54:15 -0300

    Update heroclass.js

--- heroclass.js
+++ heroclass.js
@@ -1,4 +1,3 @@
-// Definindo a classe Heroi
 class Heroi {
     constructor(nome, idade, tipo) {
       this.nome = nome;
@@ -9,7 +8,6 @@ class Heroi {
     atacar() {
       let ataque = "";
   
-      // Definindo o tipo de ataque com base no tipo do herói
       switch (this.tipo) {
         case "mago":
           ataque = "usou magia";
@@ -27,15 +25,13 @@ class Heroi {
           ataque = "usou um ataque genérico";
       }
   
-      // Exibindo a mensagem de ataque
       console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
     }
   }
   
-  // Exemplo de utilização das classes
   const heroiMago = new Heroi("Gandalf", 1500, "mago");
   const heroiGuerreiro = new Heroi("Aragorn", 35, "guerreiro");
   
-  heroiMago.atacar(); // Saída: O mago Gandalf atacou usando magia
-  heroiGuerreiro.atacar(); // Saída: O guerreiro Aragorn atacou usando espada
+  heroiMago.atacar(); 
+  heroiGuerreiro.atacar(); 
   
